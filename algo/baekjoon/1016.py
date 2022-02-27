dc5049ddf094e5b8d575f71f72f666b436c5a640
yoyoo <mcwatney@naver.com>
2022-02-27 22:40:51 +0900

    1016

--- algo/baekjoon/1016.py
+++ algo/baekjoon/1016.py
@@ -1,25 +1,9 @@
 from time import time
 
-# start_time = time()
-# minn, maxx = map(int,input().split())
-# a = [i**2 for i in range(2,int(maxx**0.5)+1)]
-# num = [1] * (maxx-minn+1)
-# for i in a:
-#     n = minn//i*i
-#     while(n < maxx+1):
-#         if n - minn >= 0:
-#             num[n-minn] = 0
-#         n += i
-# print(sum(num))
-#
-# end_time = time()
-# print(end_time - start_time)
-
 import math
 
 min, max = map(int, input().split())
 
-A = list(range(min, max+1))
 A = dict()
 for i in range(min, max + 1):
     A[i] = i
@@ -46,9 +30,7 @@ for prime_number in array: # 8만개
     a = math.ceil(min/key)
     b = max//key
     for i in range(a, b+1): # 25만개
-        print("i : ",i)
         A[key * i] = 0
-        # A[(key*i)-min]=0
 print(len(set(A.values())) - 1)
 
 
